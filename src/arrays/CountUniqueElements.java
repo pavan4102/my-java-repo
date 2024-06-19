dc683d7e34e63fbf2475cf72ce416568601ae53f
Pavan <pavanyv2002@gmail.com>
2024-06-19 23:52:19 +0530

    array programs

---
+++ src/arrays/CountUniqueElements.java
@@ -0,0 +1,35 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class CountUniqueElements {
+    public static void Unique(int[] arr){
+        int count=0;
+        int count2 = 0;
+        for(int i=0;i<arr.length-1;i++){
+                if(arr[i] == arr[i+1]){
+                    count++;
+                }
+                else{
+                    if(count == 1)
+                    count2++;
+                    count=1;
+                }
+            }
+            if(count == 1){
+                count2++;    
+            }
+            System.out.println(count2);
+        }
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter array elements");
+        for(int i=0;i<n;i++){
+        arr[i]=scan.nextInt();
+    }
+        Unique(arr);
+         }
+    }
+
---
+++ src/arrays/LargestUniqueNum.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class LargestUniqueNum {
+    public static void Largest(int[] arr){
+        int count = 1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
+            } else {
+                if (count == 1) {
+                    System.out.println("The largest number among the unique elements in array is " + arr[i + 1]);
+                    break;
+                }
+                count = 1;
+            }
+        }
+}
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter array elements");
+        for(int i=0;i<n;i++){
+        arr[i]=scan.nextInt();
+    }
+        Largest(arr);
+         }
+    }
+
