58c5f41bc291637428bcb2b36a83f41f12bd538a
Pavan <pavanyv2002@gmail.com>
2024-07-03 14:18:51 +0530

    string program

--- src/strings/AlphaSumNumeric.java
+++ src/strings/AlphaSumNumeric.java
@@ -1,30 +1,33 @@
 package strings;
 
+// Write a program to remove the special charecter from string and add the sum of numbers at the end
 import java.util.Scanner;
 
 public class AlphaSumNumeric {
 
     public static void countAll(String s) {
         String t = "";
-        int sum=0;
+        int sum = 0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ) {
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
                 t = t + s.charAt(i);
+            }
+            if (ch >= '0' && ch <= '9') {
+                sum += s.charAt(i) - 48;
+            }
         }
-        else if(ch>='0' && ch<='9'){
-            sum=sum+ch-48;
-        }
+        t = t + sum;
+        System.out.println("The new string : " + t);
+
     }
-        System.out.println(t+sum);
- }
 
     /*
      * Input:
      * Enter the string :
-     * he##o
+     * he7ll$#6o5
      * Output:
-     * The new string : heo
+     * The new string : hello18
      */
 
     public static void main(String[] args) {
@@ -35,4 +38,3 @@ public class AlphaSumNumeric {
         scan.close();
     }
 }
-
